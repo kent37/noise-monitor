8d138da79cf5837448765fa9c21a5092d8dd222a
Kent Johnson <kent3737@gmail.com>
2015-08-14 12:41:02 -0400

    Initial checkin
    Code from Rene as of 8/13/2015

---
+++ logging/logger_network.py
@@ -0,0 +1,190 @@
+
+# Sound/Noise logger - networked
+# 2015, Rene Vega
+# 2015-08-08, Fix registration logic.
+# 2015-08-09, Add try/except on http request to catch connection errors
+#
+# This applet processes all the log files found in the logs directory.
+# It sends the data to the common server for safekeeping and analysis.
+# It can be restarted at any time. Old files are sent in bulk. The
+# active log file is sent up to its end and then a real-time poll
+# occurs to grab any new data dumped into the log file by the logger
+# applet. If further local processing of the data is desired, start
+# this applet with the -bkup parameter:
+# sudo python logger_network.py -bkup
+#
+
+import time
+import logging
+import json
+import ssl
+import requests
+import os
+import datetime
+import sys
+
+# server connection
+URL = 'https://skyote.com/nl/nl-serverj.php'
+PK  = '569gksxi4e7883,r60etgv-10d'
+RefID = ''
+
+# Raspbian linux has somewhat broken SSL that is going to be fixed soon,
+# so in the meantime, disable the warning messages
+logging.captureWarnings(True)
+
+# The requests package will maintain keep-alive connections for a session,
+# vastly speeds up HTTP accesses.
+S = requests.Session()
+
+def main():
+   global URL
+   global PK
+   global RefID
+   global Backup
+
+   # Check for -bkup parameter
+   if len(sys.argv) > 1 and sys.argv[1] == '-bkup':
+      Backup = True
+      print 'Creating backup log files'
+   else: Backup = False
+
+   # Create the bkup directory
+   dir = 'bkup'
+   if not os.path.exists(dir):
+      os.makedirs(dir)
+
+   # Query the server's time (diagnostic)
+   query_args = { 'Action':'get-time', 'PassKey':PK }
+   try:
+      response = S.post(URL, data=json.dumps(query_args))
+      print response.content
+   except:
+      print 'Server not responding'
+      sys.exit(1)
+
+   # Register using the serial number of the RPI
+   # This needs to be done at least once to create a device entry
+   serial = GetSerial()
+   query_args = { 'Action':'register', 'Serial':serial, 'PassKey':PK }
+   response = S.post(URL, data=json.dumps(query_args))
+   print response.content
+
+   # Log in using the Raspberry PI's serial number. A reference ID is returned
+   query_args = { 'Action':'data-login', 'Serial':serial, 'PassKey':PK }
+   response = S.post(URL, data=json.dumps(query_args))
+   if response.status_code != 200:
+      print 'Server rejected login, code=', response.status_code
+      sys.exit(1)
+   jresp = json.loads(response.content)
+   if jresp['Status'] == 'OK': RefID = jresp['Ref_ID']
+   else: RefID = ''
+   print 'Ref_ID=', RefID, ' ',response
+
+   print 'Network logger started'
+
+   foff = 0
+   lastlog = ''
+   delay = 10
+   while True:
+      # Send the log files to the server. Delete them when the
+      # entire contents have been sent.
+      while True:
+         logf = GetLogFiles()
+         if logf is None: break
+
+         nlogs = len(logf)
+         # if this is an archived log (not the active one), start
+         # from the beginning of the file
+         if len(logf) != 1: foff = 0;
+         if lastlog != logf[0]:
+            print 'Processing log file (of ', nlogs, '): ', logf[0], ' @=', foff
+            lastlog = logf[0]
+         foff = ProcessLogFile(logf[0], foff)
+
+         # positive offset means the file was completely processed.
+         # Usually that means the log file should be deleted or backed up,
+         # but when only one file is present, it is the active one and needs
+         # to be left intact.
+         if (foff > 0):
+            if nlogs != 1:
+               if Backup: os.rename('logs/' + logf[0], 'bkup/' + logf[0])
+               else:      os.remove('logs/' + logf[0])
+               foff = 0
+            else:
+               print 'real-time', datetime.datetime.now()
+               time.sleep(delay)
+
+         # A negative offset means the file was not completely processed.
+         # This usually indicates a network connection problem, so set up
+         # for a retry.
+         else:
+            foff = -foff
+            break
+
+      time.sleep(10)
+
+# Get the serial number of the Raspberry PI
+def GetSerial():
+   serial = ''
+   f = open('/proc/cpuinfo', 'r')
+   for l in f:
+      if l[0:6] == 'Serial': serial = l[10:26]
+   f.close()
+   return serial
+
+# Get the sorted list of log file name
+def GetLogFiles():
+   logf = os.listdir('logs')
+   logf.sort()
+   return logf
+
+def ProcessLogFile(logf, foff):
+   # Send each item of the file to the server.
+   # If the server times out, indicate failure
+   ofoff = foff
+   maxts = ''
+
+   f = open('logs/' + logf, 'r')
+   if f is None: return None;
+   if foff != 0: f.seek(foff)
+
+   cnt = 0
+   for line in f:
+      foff = f.tell()
+      items = line.split(',')
+      # if foff != 0: print 'offset=', foff, 'line=', line
+
+      if len(items) > 2:
+         if (maxts == '') | (items[0] > maxts):
+            # timestemp, meter_model, weight, rate, mode, range, num_samp, db1, db2, ...
+            # 2015-08-01 14:12:34.440317,WENSN 1361,A,fast,samp,30-80db,2,32.8,35.2
+            query_args = { 'Action':'log', 'PassKey':PK, 'Ref_ID':RefID,
+                           'Timestamp':items[0], 'Meter':items[1],
+                           'Weighting':items[2],'TimeZone':'-8', 'Samples':items[6],
+                           'Data':",".join([str(item) for item in items[7:]]) }
+            try:
+               response = S.post(URL, data=json.dumps(query_args))
+               if response.status_code != 200:
+                  f.close()
+                  return -foff;
+            except:
+               f.close()
+               return -foff;
+
+            # if the server tells us this was a duplicate record, get the maximum timestamp
+            # then use it to skim through the file. This avoids swamping
+            # the server with wasteful transactions.
+            jresp = json.loads(response.content)
+            if jresp['Status'] == 'Duplicate':
+               maxts = jresp['Timestamp']
+               print 'Duplicate, skipping to', maxts
+
+      else: print 'Tossing ', foff, ':', line
+
+      cnt += 1
+      if (cnt %  100) == 0: print 'Processed: ', cnt
+
+   f.close()
+   return foff;
+
+main()
---
+++ logging/logger_wensn.py
@@ -0,0 +1,127 @@
+
+# Sound/noise logger for WENSN 1361 meter
+# 2015, Rene Vega
+# 2015/08/02 - switch to new log file every two hours
+
+import datetime
+import time
+import sys
+import usb.core
+import os
+
+def main():
+   # Print all usb devices (diagnostic)
+   dev = usb.core.find(find_all=True)
+   for cfg in dev:
+      sys.stdout.write('vid=' +hex(cfg.idVendor) +' pid=' +hex(cfg.idProduct) +'\n')
+
+   # Create the Log directory if not found
+   dir = 'logs/'
+   if not os.path.exists(dir):
+      os.makedirs(dir)
+
+   while True:
+      # Locate the WENSN meter. If it is not found do a slow poll looking for it.
+      print 'Locating WENSN meter...'
+      dev = None
+
+      try:
+         while dev is None:
+            dev = usb.core.find(idVendor=0x16c0, idProduct=0x5dc) # WENSN
+            if dev is not None:
+               print 'device found'
+               break
+            time.sleep(1.0)
+      except KeyboardInterrupt:
+         print '\nEnding search'
+         sys.exit(0)
+
+      dev.set_configuration()
+
+      # Make the log name, then open it.
+      dt = str(datetime.datetime.now()).replace(' ','_')
+      logname = dir + 'Log_' + dt
+      print 'getting db data & logging to: ', logname
+      logfile = open(logname, 'a')
+
+      # Gather the meter data, sampling twice a second.
+      # Also decode the meter options
+      try:
+         if GetSamples(dev,logfile) == False:
+            print '/nSampling ended'
+            logfile.close()
+            sys.exit(0)
+
+      except KeyboardInterrupt:
+         print 'Ending noise sampling'
+         logfile.close()
+         sys.exit(0)
+
+      except Exception as e:
+         print 'Exception accesing meter: ', e
+
+
+      logfile.close()
+      print 'Restarting data collection'
+      # ...and loop we go
+
+# ---------------------------------------
+def GetSamples(dev,logfile):
+   tbgn = datetime.datetime.now()
+   dbi = 0
+   db = []
+   while True:
+      now = datetime.datetime.now()
+      opts = ''
+      meter = 'WENSN 1361'
+      weight = ''
+      rate  = ''
+      mode  = ''
+      range = ''
+
+      # Grab meter data
+      ret = dev.ctrl_transfer(0xC0, 4, 0, 0, 2, 1000)
+      if ret is None: return False
+
+      # Decode the options flags
+      ro = ret[1] & 0xfc
+      if (ro&0x20) != 0: weight='C'
+      else:              weight='A'
+      if (ro&0x40) != 0: rate ='slow'
+      else:              rate ='fast'
+      if (ro&0x80) != 0: mode ='max'
+      else:              mode = 'samp'
+
+      # Decode the range settings
+      ro = ro & 0x1f
+      if   ro == 0x00: range ='30-80db'
+      elif ro == 0x04: range ='40-90db'
+      elif ro == 0x08: range ='50-100db'
+      elif ro == 0x0c: range ='60-110db'
+      elif ro == 0x10: range ='70-120db'
+      elif ro == 0x14: range ='80-130db'
+      elif ro == 0x18: range ='30-130db'
+      opts = weight +','+rate +','+mode +','+range
+
+      # Decibels is a 10-bit value with zero representing 30db
+      db.append((ret[0] + ((ret[1] & 3) * 256)) * 0.1 + 30)
+      dbi += 1
+
+      if dbi > 1:
+         print db[0], ',', db[1], ' (', opts, ') raw=', str(bytearray(ret)).encode('hex')
+
+         # Write a log entry: datetime,meter_type,weight,rate,mode,range,num_samples,samp1,samp2,...
+         # Flush the buffer on every write to ensure the network logger sees timely data
+         logentry = str(datetime.datetime.now())+','+meter+','+opts+','+str(dbi)+','+str(db[0])+','+str(db[1])+'\n'
+         logfile.write(logentry)
+         logfile.flush()
+         dbi = 0
+         db = []
+
+      tdelta = now - tbgn
+      esec = tdelta.total_seconds()
+      if esec > 7200: return True
+
+      time.sleep(0.5)
+
+main()
