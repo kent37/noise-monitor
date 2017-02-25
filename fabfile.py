0de1f98224a80425f3b755fb26e31d4babdc014f
Kent Johnson <kent3737@gmail.com>
2017-02-25 15:43:22 -0500

    Fetch tracks to USB stick

--- fabfile.py
+++ fabfile.py
@@ -44,20 +44,22 @@ def count_new_tracks():
 	''' Count the number of tracks which have not been downloaded yet. '''
 	print len(find_new_tracks()), ' new tracks'
 
+local_track_dir = '/Volumes/TRACKS/'
+
 @task
 @hosts('rpi2.local')
 def get_new_tracks():
 	''' Download all new tracks. '''
 	new_tracks = find_new_tracks()
-	print 'Fetching', len(find_new_tracks()), ' new tracks'
-	with cd('/home/pi/logger/tracks'), lcd('../tracks'):
+	print 'Fetching', len(new_tracks), ' new tracks'
+	with cd('/home/pi/logger/tracks'), lcd(local_track_dir):
 		for track in new_tracks:
 			print track
 			get(remote_path=track, local_path=track)
 
 def find_new_tracks():
 	''' Get a list of tracks on the remote not on local. '''
-	local_tracks = set(os.listdir('../tracks'))
+	local_tracks = set(os.listdir(local_track_dir))
 	with cd('/home/pi/logger/tracks'):
 		myout = StringIO()
 		output = run('ls', stdout=myout)
